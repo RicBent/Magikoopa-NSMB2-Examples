bb3ddf266b062378c7289626192d3678f1665ed0
RicBent <bent@mail.de>
2017-10-24 00:39:38 +0200

    Added BlockItem example

---
+++ BlockItem/blockItem.cpp
@@ -0,0 +1,24 @@
+enum BlockContents
+{
+    BC_NOTHING              = 0x00,
+    BC_COIN                 = 0x01,
+    BC_FIREFLOWER           = 0x02,
+    BC_NOTHING_2            = 0x03,
+    BC_LEAF                 = 0x04,
+    BC_GOLDENFLOWER         = 0x05,
+    BC_MINIMUSHROOM         = 0x06,
+    BC_STAR                 = 0x07,
+    BC_COIN_2               = 0x08,
+    BC_NOTHING_3            = 0x09,
+    BC_MULTIPLECOINS        = 0x0A,
+    BC_LIFE                 = 0x0B,
+    BC_NOTHING_4            = 0x0C,
+    BC_SPRING               = 0x0D,
+    BC_MUSHROOMIFSMALL      = 0x0E,
+    BC_NOTHING_5            = 0x0F
+};
+
+unsigned int BlockSetItem()
+{
+    return BC_GOLDENFLOWER;
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Magikoopa
+
+Examples for using Magikoopa (https://github.com/RicBent/Magikoopa/) together with New Super Mario Bros. 2 to get full custom code.
+
+### BlockItem
+* All sprite blocks spawn a given item
+* Shows how to replace an entire game function
